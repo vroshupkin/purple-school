82c8384c45a33092b4fc3add5b6495c460518096
vroshupkin <vroshupkin1@gmail.com>
2022-11-01 01:35:18 +0300

    Завершен 3ий модуль: Начало работы с Node.js

--- 3/events/app.js
+++ 3/events/app.js
@@ -28,21 +28,37 @@ myEmitter.emit('off')
 // по-умолчанию 10 максимальных слушателей на событие
 console.log(myEmitter.getMaxListeners())
 
-myEmitter.on('msg', (data) => {
-    console.log(`Получил: ${data}`)
-})
-
-myEmitter.on('msg', (data) => {
-    console.log(`Второй слушатель получил: ${data}`)
-})
+myEmitter.on('msg', (data) => console.log(`Получил: ${data}`))
+myEmitter.on('msg', (data) => console.log(`Второй слушатель получил: ${data}`))
+myEmitter.prependListener('msg', (data) => console.log('Слушатель добавленный первым в список обработчиков!'))
 
 // #3 Срабатывание события с отправкой текста
 myEmitter.emit('msg', 'Привет: это эммитер!')
 
-
+// Все слушатели события msg
+console.log(myEmitter.listeners('msg'))
 console.log(myEmitter.listenerCount('msg'))
 
+// Название активных событий в эмитере
+console.log(myEmitter.eventNames())
 
+
+// Слушатель ошибки является её обработчиком
+myEmitter.on('error', (err) => {
+    console.log(`Произошла ошибка: ${err.message}`)
+})
+
+myEmitter.emit('error', new Error('Boom!'))
+
+
+// #Пример с eventTarget
+const target = new EventTarget()
+const logTarget = () => {
+    console.log('Connect with event target')
+}
+
+target.addEventListener('connect', logTarget)
+target.dispatchEvent(new Event('connect'))
 
 
 
